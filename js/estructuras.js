de30c94fd2b27cc45bb2cec2f720c2bce34cfe1c
coredummie2020 <coredummie2020@gmail.com>
2020-07-15 11:28:37 +0200

    'estructuras'

---
+++ js/estructuras.js
@@ -0,0 +1,157 @@
+/* Estructuras de control */
+
+// Estructura condicional if
+
+// if (condición) {
+    // código a ejecutar si se cumple la condición
+// }
+
+let nombre = "Laura";
+let edad = 20;
+let mayorEdad;
+
+if (edad >= 18 && nombre !== undefined) {
+    mayorEdad = true;
+    console.log(nombre + ' es mayor de edad');
+}
+
+if (mayorEdad)
+    console.log('Es mayor de edad'); // No se suele emplear el alivio de las llaves
+
+// Estructura condicional if else 
+
+// if (condición) {
+    // código a ejecutar si se cumple la condición
+// } else {
+    // código a ejecutar si no se cumple la condición
+// }
+
+let a = 10;
+let b = 20;
+
+if (a > b) {
+    console.log('a es mayor que b');
+} else {
+    console.log('a es menor o igual que b');
+}
+
+// Estructura if else if else
+
+// if (condición1) {
+    // código a ejecutar si se cumple la condición1
+// } else if (condición2) {
+    // código a ejecutar si se cumple la condición2
+// } else {
+    // código a ejecutar si no se cumple ninguna de las condiciones
+// }
+
+if (a > b) {
+    console.log('a es mayor que b');
+} else if (a === b) {
+    console.log('a es igual a b');
+} else {
+    console.log('a es menor que b');
+}
+
+// Las estructuras if else se pueden anidar
+
+// Estructuras condicionales switch
+
+// switch (expresión) {
+    // case valor1:
+        // código a ejecutar
+        // break
+    // case valor2:
+        // código a ejecutar
+        // break
+    // ...
+    // default
+        // código a ejecutar en caso de no coincidencia
+// }
+
+let diaSemana = 2;
+
+switch (diaSemana) {
+    case 1:
+       console.log('Lunes');
+       break;
+    case 2:
+       console.log('Martes');
+       break;
+    case 3:
+       console.log('Miércoles');
+       break;
+    case 4:
+       console.log('Jueves');
+       break;
+    case 5:
+       console.log('Viernes');
+       break;
+    case 6:
+       console.log('Sábado');
+       break;
+    case 7:
+       console.log('Domingo');
+       break;
+    default:
+        console.log('Día erróneo');
+}
+
+let mes = 4;
+
+switch (mes) {
+    case 1:
+    case 2:
+    case 3:
+        console.log('1er Trimestre');
+        break;
+    case 4:
+    case 5:
+    case 6:
+        console.log('2º Trimestre');
+        break;
+    case 7:
+    case 8:
+    case 9:
+        console.log('3er Trimestre');
+        break;
+    case 10:
+    case 11:
+    case 12:
+        console.log('4º Trimestre');
+        break;
+    default:
+        console.log('Mes erróneo');
+}
+
+// Estructura iterativa For
+
+// for (valor inicial; condición; actualización) {
+//    código a ejecutar mientras se cumpla la condición
+// }
+
+for (i = 0; i < 10; i++) {
+    console.log('En cada iteración i tendrá el valor ' + i);
+}
+
+// Estructura iterativa While
+
+// while (condicion) {
+    // código a ejecutar mientras se cumpla esa condición
+// }
+
+let password;
+
+// while (password !== '1234') {
+//     password = prompt('Escriba su contraseña');
+// }
+
+// Estructura iterativa do while
+
+// do {
+//     código a ejecutar mientras se cumpla la condición   
+// } while (condición)
+
+do {
+    password = prompt('Escriba su contraseña');
+} while (password !== '1234');
--- js/operadores.js
+++ js/operadores.js
@@ -14,7 +14,174 @@ let total = mensaje * puntuacion; // devuelve NaN not a number
 
 // console.log(total);
 
-let  resultado = "12";
+let resultado = "12";
 let resultado2 = "20";
 
 console.log(resultado + resultado2);
+
+// Negación unaria
+
+let resultado3 = 6;
+
+let resultado4 = -resultado3;
+
+console.log(resultado4);
+
+// Unario positivo
+
+let mayorEdad = true;
+
+console.log(+mayorEdad);
+
+// Resto %
+
+let resultado5 = 13 % 2;
+
+console.log(resultado5);
+
+// Incremento ++ (suma una unidad al valor de la variable)
+
+let resultado6 = 5;
+
+resultado6++;
+
+console.log(resultado6);
+
+// Pre-incremento
+
+let resultado7 = 10;
+
+console.log(resultado7++);
+console.log(resultado7);
+
+let resultado8 = 20;
+
+console.log(++resultado8);
+console.log(resultado8);
+
+// Decremento -- (resta una unidad al valor de la variable)
+
+// Idem incremento
+
+// Exponenciación ** ECMA6
+
+let resultado9 = 2 ** 3;
+
+console.log(resultado9);
+
+// Precedencia de operaciones
+
+// De izquierda a derecha y las mismas precedencias que en
+// operaciones matemáticas
+// La precedencia se rompe con paréntesis
+
+let resultado10 = (12 + 3) * 8;
+
+console.log(resultado10);
+
+/* Operadores de asignación */
+
+// Asignación simple =
+
+let resultado11 = 20;
+
+// Asignación de adicción +=
+
+let resultado12 = 10;
+
+console.log(resultado11 += resultado12); // resultado11 = resultado11 + resultado12;
+
+// Asignación de resta -=
+//     "      de multiplicación *=
+//     "      de división /=
+//     "      de resto %=
+//     "      de exponenciación **=      x **= y;   x = x ** y;
+
+
+/* Operadores de comparación */
+
+// Igualdad ==
+
+let ciudad = 'Getafe';
+let termino = 'Getafe';
+
+let existeCoincidencia = ciudad == termino;
+
+console.log(existeCoincidencia);
+
+let password = '1234';
+let rePassword = 1234;
+
+let checkPassword = password == rePassword;
+
+console.log(checkPassword);
+
+// Igualdad estricta === (Comprueba tanto valores como tipos)
+
+checkPassword = password === rePassword;
+
+console.log(checkPassword);
+
+// Desigualdad !=
+
+// Desigualdad estricta !==
+
+// Mayor que >
+
+const minimaPuntuacion = 50;
+let puntuacion2 = 80;
+
+let superado = puntuacion2 > minimaPuntuacion;
+
+console.log(superado);
+
+// Mayor o igual >=
+
+puntuacion2 = 50;
+superado = puntuacion2 >= minimaPuntuacion;
+
+console.log(superado);
+
+// Menor <
+
+// Menor o igual <= (Comprueba lexicográficamente Símbolos, Números, Mayúsculas, Minúsculas)
+
+let nombre1 = 'Pepe';
+let nombre2 = 'Luis';
+
+console.log(nombre1 < nombre2);
+
+/* Operadores lógicos */
+
+// And lógico &&
+
+let valor1 = 12;
+let valor2 = 14;
+let valor3 = 12;
+
+resultado = valor2 > valor1 && valor1 !== valor3;
+console.log(resultado);
+
+// Or lógico ||  
+
+resultado = valor2 > valor1 || valor1 !== valor3;
+console.log(resultado);
+
+
+// Not lógico !
+
+mayorEdad = false;
+
+mayorEdad = !mayorEdad;
+
+console.log(!mayorEdad);
+
+// Operador condicional (ternario)
+
+// expresion-condicional ? expresion-si-cumple : expresion-no-cumple
+
+edad = 17;
+
+mayorEdad = edad >= 18 ? 'Adulto' : 'Menor';
+
+console.log(mayorEdad);
