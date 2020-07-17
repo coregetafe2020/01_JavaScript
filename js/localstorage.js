e9272e8cbc84b02eaf32e91d780277a57d9cdce1
coredummie2020 <coredummie2020@gmail.com>
2020-07-17 12:37:43 +0200

    'local'

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,3 +1,5 @@
+// Objeto JSON para parseo
+
 let jugador = {
     nombre: 'Cristiano',
     apellidos: 'Ronaldo',
@@ -9,3 +11,17 @@ console.log(jugadorEnJson);
 
 let nuevoJugador = JSON.parse(jugadorEnJson);
 console.log(nuevoJugador);
+
+// localStorage de navegadores
+
+let participantes = [
+    {nombre: "Juan", apellidos: "López"},
+    {nombre: "Laura", apellidos: "Peŕez"}
+];
+
+localStorage.setItem('participantes', JSON.stringify(participantes));
+
+let nuevosParticipantes = JSON.parse(localStorage.getItem('participantes'));
+console.log(nuevosParticipantes);
+
+// localStorage.removeItem('participantes');
